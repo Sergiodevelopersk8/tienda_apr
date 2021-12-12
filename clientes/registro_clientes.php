3222ae5f4c79f0b67443c2750e52d3cca38499d6
SergioMerinoCortez <utp0001553@alumno.utpuebla.edu.mx>
2021-12-11 22:06:38 -0600

    se cambia la primera letra del nombre en mayuscula y la del apellido tambien

--- clientes/registro_clientes.php
+++ clientes/registro_clientes.php
@@ -18,8 +18,8 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 	
 //------------------------------------------------------//
 	
-	//$nombre=ucwords($nombre);
-	//$apellidos=ucwords($apellidos);
+	$nombre=ucwords($nombre);
+	$apellidos=ucwords($apellidos);
 	
 	
 	$nombre=utf8_decode($nombre);
